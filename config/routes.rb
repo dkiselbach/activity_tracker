5d518aca3a9ef607b813f4911deae9798807583f
dkiselbach <dylankiselbach@gmail.com>
2020-05-09 10:13:06 -0700

    Added static pages and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get  '/home',    to: 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/setup',    to: 'static_pages#setup'
   devise_for :users
-  root 'application#hello'
 end
