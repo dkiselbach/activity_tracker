f63ad90b777687edc625fada4e152185bcd0e24c
dkiselbach <dylankiselbach@gmail.com>
2020-05-09 09:05:43 -0700

    Added correct domain to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'agile-retreat-42559'
+  host = 'agile-retreat-42559.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
