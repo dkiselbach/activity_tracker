39b6132285000ac5c48499619369dcfd0a01faf1
dkiselbach <dylankiselbach@gmail.com>
2020-05-09 10:58:41 -0700

    added a dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/profile'
+  get 'user/activities'
   root 'static_pages#home'
   get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/setup',    to: 'static_pages#setup'
   devise_for :users
+  resources :dashboard,     only: [:show]
 end
