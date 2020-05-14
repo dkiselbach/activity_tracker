26950ec98ee744f7a062173fd32d39502d79b4ed
dkiselbach <dylankiselbach@gmail.com>
2020-05-14 12:43:24 -0700

    fix migration 2

--- db/migrate/20200514165514_change_data_type_for_time.rb
+++ db/migrate/20200514165514_change_data_type_for_time.rb
@@ -1,8 +1,8 @@
 class ChangeDataTypeForTime < ActiveRecord::Migration[6.0]
-  def up
-    add_column :activities, :time, :integer
-  end
   def down
     remove_columns :activities, :time, :datetime
+  end
+  def up
+    add_column :activities, :time, :integer
   end
 end
