77d4e7788d6e66bf7f8fdd14c7d1c34edfe5635e
dkiselbach <dylankiselbach@gmail.com>
2020-05-14 12:09:17 -0700

    fix migration

--- db/migrate/20200514165514_change_data_type_for_time.rb
+++ db/migrate/20200514165514_change_data_type_for_time.rb
@@ -1,12 +1,5 @@
 class ChangeDataTypeForTime < ActiveRecord::Migration[6.0]
-    def self.up
-    change_table :activities do |t|
-      t.change :time, :integer
+    def change
+      change_column :activites, :time, 'integer USING CAST(time AS integer)'
     end
   end
-  def self.down
-    change_table :activites do |t|
-      t.change :time, :datetime
-    end
-  end
-end
