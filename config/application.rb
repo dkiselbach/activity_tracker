a75ca633bd93227b56acc3e092acd625db0cde07
dkiselbach <dylankiselbach@gmail.com>
2020-05-19 09:59:50 -0700

    Update Cors

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ActivityTracker
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource( '/api/v1/*',
+        resource( '*',
           headers: :any,
           expose: ["Authorization"],
           methods: [:get, :patch, :put, :delete, :post, :options, :show]
