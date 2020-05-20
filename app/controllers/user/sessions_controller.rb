ebf6dda33a977fb60099f9bca49eaac37c3fa195
dkiselbach <dylankiselbach@gmail.com>
2020-05-20 09:44:34 -0700

    Added extra login logic

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -4,14 +4,23 @@ class User::SessionsController < Devise::SessionsController
   wrap_parameters :user
 
   def create
-      warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-      render :json => { :success => true,
-                        :info => "Login successful" }
+      self.resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+      render :status => 200,
+             :json => { :success => true,
+                        :info => "Login successful",
+                        :email => resource.email,
+                        :name => resource.name}
   end
 
   def failure
-    render :status => :unauthorized,
-           :json => { :success => false,
-                      :info => "Login failed" }
+    if User.find_by(email: sign_in_params[:email])
+      render :status => :unauthorized,
+             :json => { :success => false,
+                        :info => "The password you’ve entered is incorrect." }
+    else
+      render :status => :unauthorized,
+             :json => { :success => false,
+                        :info => "The email you’ve entered doesn’t match any account." }
+    end
   end
 end
