abf6641bf6e17eeedc8d5144c5784240eced67ca
dkiselbach <dylankiselbach@gmail.com>
2020-06-05 06:49:26 -0700

    updated user show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,13 @@ class Api::V1::UsersController < ApplicationController
     render :json => current_user.as_json.merge(:profile_image => profile_image_url)
   end
 
+  def show
+    @user = User.find(params[:id])
+    longest_run = Activity.exclude_laps_splits.where(activity_type: "Run").order("distance DESC").first
+    longest_ride = Activity.exclude_laps_splits.where(activity_type: "Ride").order("distance DESC").first
+    render :json => @user.as_json.merge(:longest_run => longest_run, :longest_ride => longest_ride)
+  end
+
   def update
     if current_user.update(user_params)
       render json: current_user
