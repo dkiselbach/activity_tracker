57545f697550618aad20e3d440628b94bed54085
dkiselbach <dylankiselbach@gmail.com>
2020-06-08 07:54:41 -0700

    fix users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,21 @@ class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render :json => current_user.as_json.merge(:profile_image => profile_image_url)
+    @user = current_user
+    longest_run = Activity.exclude_laps_splits.where(activity_type: "Run").order("distance DESC").first
+    longest_ride = Activity.exclude_laps_splits.where(activity_type: "Ride").order("distance DESC").first
+    #fastest_5k = Activity.exclude_laps_splits.where(activity_type: "Run").where("distance > ?", 20960).order("speed DESC").first
+    render :json => @user.as_json.merge(:longest_run => longest_run, :longest_ride => longest_ride,
+      :profile_image => profile_image_url)
   end
 
   def show
     @user = User.find(params[:id])
     longest_run = Activity.exclude_laps_splits.where(activity_type: "Run").order("distance DESC").first
     longest_ride = Activity.exclude_laps_splits.where(activity_type: "Ride").order("distance DESC").first
-    fastest_5k = Activity.exclude_laps_splits.where(activity_type: "Run").where("distance > ?", 20960).order("speed DESC").first
+    #fastest_5k = Activity.exclude_laps_splits.where(activity_type: "Run").where("distance > ?", 20960).order("speed DESC").first
     render :json => @user.as_json.merge(:longest_run => longest_run, :longest_ride => longest_ride,
-      :profile_image => profile_image_url, :fastest_5k => fastest_5k)
+      :profile_image => profile_image_url)
   end
 
   def update
