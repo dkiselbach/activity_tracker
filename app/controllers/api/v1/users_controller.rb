9ea13aa1c553ad2ea9ff202b72e36229db109194
dkiselbach <dylankiselbach@gmail.com>
2020-06-12 07:33:47 -0700

    Add prs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,9 +15,10 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
     longest_run = Activity.exclude_laps_splits.where(activity_type: "Run").order("distance DESC").first
     longest_ride = Activity.exclude_laps_splits.where(activity_type: "Ride").order("distance DESC").first
+    fastest_half_marathon = Record.where(name: "Half-Marathon").where(pr_rank: 1).order("start_date_local DESC")
+    fastest_10k = Record.where(name: "10k").where(pr_rank: 1).order("start_date_local DESC")
     #fastest_5k = Activity.exclude_laps_splits.where(activity_type: "Run").where("distance > ?", 20960).order("speed DESC").first
-    render :json => @user.as_json.merge(:longest_run => longest_run, :longest_ride => longest_ride,
-      :profile_image => profile_image_url)
+    render :json => @user.as_json.merge(:longest_run => longest_run, :longest_ride => longest_ride, :fasted_half_marathon => fastest_half_marathon, :fastest_10k => fastest_10k, :profile_image => profile_image_url)
   end
 
   def update
